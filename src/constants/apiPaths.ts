1368dd3ad1adf08d8375c9bada4d477e7e084eb1
stmaslakoff <i.maslakoff@softteco.com>
2025-03-08 19:07:06 +0300

    integrates create product API endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://l5jkd362hl.execute-api.eu-central-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://yoj2g5k7hb.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://l5jkd362hl.execute-api.eu-central-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
