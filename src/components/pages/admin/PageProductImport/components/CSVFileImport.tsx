e123081ab3f4c6830a89c2984d75cd8c7c8f0aa3
stmaslakoff <i.maslakoff@softteco.com>
2025-03-24 10:50:53 +0300

    feat(task-7): Authorization
    sends auth headers | handles error responses

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -41,7 +41,6 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
           Authorization: authToken,
         },
       });
-      console.log({ response })
       console.log("File to upload: ", file.name);
       console.log("Uploading to: ", response.data);
       const result = await fetch(response.data, {
