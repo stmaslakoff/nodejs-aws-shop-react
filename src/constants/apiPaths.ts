2e52d8f170439fa7b00fefb0b168bed3baeebc8a
stmaslakoff <i.maslakoff@softteco.com>
2025-04-16 01:36:05 +0300

    feat(task-9): Docker and AWS Elastic Beanstalk
    change cart endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://yoj2g5k7hb.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://l5jkd362hl.execute-api.eu-central-1.amazonaws.com/prod",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://stmaslakoff-cart-api.eba-txb9pdk2.eu-central-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
